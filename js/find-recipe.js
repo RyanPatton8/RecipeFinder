493e74430ece5035d1b3f456b5fade560b1c6c9a
Ryan Patton <ryanpatton173@gmail.com>
2024-11-28 12:25:39 -0500

    cards are now displayed

--- js/find-recipe.js
+++ js/find-recipe.js
@@ -9,25 +9,26 @@ const searchBtn = document.querySelector("#search-full");
 const searchCharBtn = document.querySelector("#search-first-letter");
 const searchIngrBtn = document.querySelector("#search-ingredient");
 
+
 // Change the search type based on btn clicked and call fetch results
 searchBtn.onclick = function (event){
     event.preventDefault();
     searchType = "search.php?s";
-    fetchResults(searchBar.value.trim());
+    fetchResults();
 };
 searchCharBtn.onclick = function (event){
     event.preventDefault();
     searchType = "search.php?f";
-    fetchResults(searchBar.value.trim().charAt(0));
+    fetchResults();
 };
 searchIngrBtn.onclick = function (event){
     event.preventDefault();
     searchType = "filter.php?i";
-    fetchResults(searchBar.value.trim());
+    fetchResults();
 };
 
 //This function returns a list of detailed meal objects checking to see if it is a first letter only search using a terinary operator
-function searchBarPromise(searchQuery) {
+function searchBarPromise() {
     url = `${baseURL}${searchType}=${searchType === "search.php?f" ? searchBar.value.trim().charAt(0) : searchBar.value.trim()}`;
     return fetch(url)
     .then(result => {return result.json()})
@@ -81,19 +82,43 @@ async function fetchResults(){
         let wasStrInputted = searchBarResults.length > 0;
         let wasResults = finalResults.length > 0;
         if(wasResults){
-            console.log(finalResults);
+            displayResults(finalResults);
         }
         else if(wasStrInputted && wasFiltered){
-            console.log("Nothing of specified search in category");
+            displayResults("No results in category");
         }
         else if(wasStrInputted){
-            console.log(searchBarResults)
-        }
-        else if(wasFiltered){
-            console.log("Must Input Search")
+            displayResults(searchBarResults);
         }
     }
     catch(error){
-        console.log("No Results");
+        console.log(error);
+        displayResults("No results in category");
+    }
+}
+
+function displayResults(results){
+    console.log(results);
+    // Section that holds all results
+    const resultSection = document.querySelector("#meals");
+
+    while (resultSection.firstChild) {
+        resultSection.removeChild(resultSection.firstChild);
+    }
+
+    if(typeof results === "string"){
+        const paragraph = document.createElement("p");
+        paragraph.textContent = "No results in category";
+        resultSection.appendChild(paragraph);
+    }
+    else{
+        for(obj of results){
+            resultSection.innerHTML += `
+            <div class = "meal-card">
+                <img src="${obj.strMealThumb}" alt="${obj.strMeal}">
+                <p>${obj.strMeal}</p>
+            </div>
+            `;
+        }
     }
 }
